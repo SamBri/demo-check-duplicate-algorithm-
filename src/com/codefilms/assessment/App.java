afc89543835da3620d81368890eb1ea5f2de117a
Samuel Brifo <lunarhive@gmail.com>
2022-02-27 23:09:10 +0000

    add check duplicate algorithm

---
+++ src/com/codefilms/assessment/App.java
@@ -0,0 +1,76 @@
+package com.codefilms.assessment;
+
+
+
+public class App {
+
+	public static void main(String[] args) {
+	
+		
+		//simple algorithm to check for duplicate numbers
+		
+		int[] numbers = {40,22,22,22,35,9,19,82};
+			
+		checkDuplicate(numbers);
+		
+	}
+	
+	public static void checkDuplicate(int[] numbers) {
+		
+	
+		//let us have a copy of the numbers to find duplicate
+		int[] tempNumbers = numbers;
+		int duplicate;
+		int scannerHead;
+		System.out.println("Starting from left to right order(L-R) to check duplicate");
+		for(int  head = 0, end = tempNumbers.length -1; head < end ;head++)
+		{
+			//pick the first element in the array and compare to all others
+			duplicate = tempNumbers[head];
+			scannerHead = head;
+			
+			System.out.println("Scanner Head points at index:" + scannerHead);
+			System.out.println("(L-R) order <-> Find Number  : "+duplicate);
+			
+			
+			//start search of duplicate from the next element after the zero index
+			for(int scanIndex = scannerHead + 1; scanIndex < tempNumbers.length; ++scanIndex)
+			{
+				// if there are no duplicates then show the scanned numbers
+				if(duplicate != tempNumbers[scanIndex])
+			{
+				System.out.println("Scanned Number:"+ tempNumbers[scanIndex]);
+				
+				// Prompt user of no duplicate found when the scanIndex is at the end of the container.
+				if(scanIndex == tempNumbers.length)
+					System.out.println("No duplicate found");
+				
+			}
+				// for a duplicate found, show the scanned number and duplicate number which is the scanned number and the scan index of the duplicate
+				else {
+					// Tell the user the 
+					System.out.println("Scanned Number:"+tempNumbers[scanIndex] + "\nDuplicate:"+duplicate+"\nScanIndex:"+scanIndex);
+				}
+				
+
+			
+			}
+		
+		}
+		
+		
+	
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
