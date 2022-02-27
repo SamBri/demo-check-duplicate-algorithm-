7b647c27d58bc5d117a1564a3ee125bd4528fe82
Samuel Brifo <lunarhive@gmail.com>
2022-02-27 23:24:23 +0000

    rename packages and classes

---
+++ src/com/codefilms/demo/App.java
@@ -0,0 +1,30 @@
+package com.codefilms.demo;
+
+import com.codefilms.algorithm.Dataset;
+
+public class App {
+
+	public static void main(String[] args) {
+	
+		
+		
+		// sample data set to check duplicate entries:
+		// Object -> Integers
+		int[] numbers = {40,22,22,22,35,9,19,82};
+			
+		// call checkduplicate algorithm from the algorithm package
+		Dataset.checkDuplicate(numbers);
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
